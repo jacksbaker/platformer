b278f17adcf0f7b8a32e7a372b23cb3b9c7b6da4
JackBaker1 <163625@chrome.escg.ac.uk>
2021-11-08 18:10:33 +0000

    Adding playermovement

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+
+        if(Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(rb.velocity.x, 10, 0);
+        }
+    }
+}
