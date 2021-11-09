d794ca1cdbdee1d90a34480bb34e207fdf223d1b
JackBaker1 <163625@chrome.escg.ac.uk>
2021-11-09 13:12:36 +0000

    Started working on anims

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -5,25 +5,47 @@ using UnityEngine;
 public class PlayerMovment : MonoBehaviour
 {
     private Rigidbody2D rb;
-    
+    private Animator anim;
+
+    private float dirX = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
 
-        if(Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space"))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector3(rb.velocity.x, 10, 0);
         }
+
+        UpdateAnimationUpdate();
+    }
+
+    private void UpdateAnimationUpdate()
+    {
+            if (dirX > 0f)
+            {
+                anim.SetBool("Running", true);
+            }
+            else if (dirX < 0f)
+            {
+                anim.SetBool("Running", true);
+            }
+            else
+            {
+                anim.SetBool("Running", false);
+            }
+        
     }
 }
